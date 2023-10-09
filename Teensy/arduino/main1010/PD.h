217c912661ce27a3eadf0fd8e50fcb4a5045693a
Hung_Ying <frogricky@gmail.com>
2023-10-10 03:24:02 +0800

    no message

---
+++ Teensy/arduino/main1010/PD.h
@@ -0,0 +1,42 @@
+#ifndef __PD_H__
+#define __PD_H__
+#include"Arduino.h"
+
+
+class PD{
+  public:
+    PD(double, double, double, double, double, double);
+    double PIDrun(double, double);
+    //double PIDrun2(double);
+    
+    void setvariable(double, double, double, bool);
+    //double timecheck();
+    //void OutputCheck(double);
+    //void AfterVariable();
+    
+    ~PD();
+
+  
+  private:
+    double timecheck();
+    void OutputCheck(double);
+    void AfterVariable();
+    double PIDrun2(double);
+
+    double setpoint = 0;
+    double ProcessVariable = 0, ProcessVariableAfter = 0;
+    double setVariable = 0, setVariableAfter = 0;
+    double derivativeAction = 0;
+    double integratedError = 0;
+    double kp, ki, kd, AfterP, AfterI, AfterD;
+    double dt;//0.005
+    double outHIGH, outLOW, AfterOUT = 0;
+    bool reinitialize = false, first = true;
+    int startTime;
+  
+  
+  
+  
+};
+
+#endif
